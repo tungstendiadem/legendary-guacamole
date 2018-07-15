4ca9c98bb139af403c7f9b5cb6274e0e4800b276
Thomas Marshall <thomas.marshall@live.com>
2018-07-14 21:29:05 -0400

    We are working from a specification
    As stated before we are looking to create some useful functionality in multiple languages. Our first idea is to provide a string transformer which would take a string of a certain case type (camelCase, snake_case, etc.) and return a string of another case. In this commit we created some interfaces, delegates, methods, enumerations, and folders. We are tinkering with our approach. We also branched into ideas-and-approaches.

---
+++ legendary-guacamole/InCaseOf/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace InCaseOf
+{
+    public class caseDeterminer
+    {
+
+        // We either know the input type or do not. We need to know the output type.
+        private string CompareKnown (string inputType, string outputType, string inputString )
+        {
+            throw new NotImplementedException();
+        }
+
+        private string CompareUnknown   (string outputType, string inputString)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
