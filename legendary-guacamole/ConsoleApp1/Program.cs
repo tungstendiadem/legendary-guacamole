a5f00455faa33921e5a4292930fc9b8a43d4d596
Thomas Marshall <thomas.marshall@live.com>
2018-07-15 10:53:34 -0400

    Added the program.cs to use class library

---
+++ legendary-guacamole/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using InCaseOf;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+            Console.WriteLine("Hello World!");
+      
+            caseDeterminer x = new caseDeterminer();
+            bool c = x.CaseEquivalent("bTard");
+
+            Console.WriteLine(c.ToString());
+
+            bool d = x.CaseEquivalent("b_tard");
+
+            Console.WriteLine(d.ToString());
+
+        }
+    }
+}
